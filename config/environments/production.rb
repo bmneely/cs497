ee163bbfc09841aa0db9bc2ec2968160dd6f11f1
Ben Neely <ben@bloc.io>
2015-10-08 20:00:15 -0600

    Update default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'vgew.herokuapp.com' }
 end
