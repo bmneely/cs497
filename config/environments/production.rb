ad5141cd3afcbc8a77f1b24dfa60fcd40efb3ff8
Ben Neely <ben@bloc.io>
2015-12-03 17:44:28 -0700

    Heroku assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'vgew.herokuapp.com' }
-  config.serve_static_assets = true
+  config.serve_static_files = true
 end
